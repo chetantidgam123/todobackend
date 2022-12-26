f048ef82880d74e92adcafc6d0a53adae0b16499
chetan <ctidgam1997@gmail.com>
2022-12-26 22:28:06 +0530

    cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const connect = require('./db/db')
+const cors = require('cors')
+
 const app = express()
+app.use(cors({origin:true}))
 app.use(express.json())
 app.use(require("./routes/route"))
-
-app.listen('3002',async()=>{
+app.listen('3001',async()=>{
     await connect()
     console.log('serve listen on http://localhost:3001')
     
