e56baa36de44329e7472b381c98794322c0cbc4c
chetan <ctidgam1997@gmail.com>
2022-12-27 00:04:39 +0530

    origin sites added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const connect = require('./db/db')
 const cors = require('cors')
 
 const app = express()
-app.use(cors({origin:true,credentials:true}))
+app.use(cors({origin:["http://localhost:3000","https://melodious-twilight-b98265.netlify.app"],credentials:true}))
 app.use(express.json())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
