d1da2235630b73e50b73f26d97f8b88c0d177e3b
chetan <ctidgam1997@gmail.com>
2022-12-27 22:06:35 +0530

    finish

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.urlencoded({extended:false}))
-app.use(cors({origin:["http://localhost:3000","https://melodious-twilight-b98265.netlify.app"],credentials:true}))
-app.use(express.json())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   })
+app.use(cors({origin:["http://localhost:3000","https://melodious-twilight-b98265.netlify.app"],credentials:true}))
+app.use(express.json())
 app.use(require("./routes/route"))
 app.listen('3001',async()=>{
     await connect()
