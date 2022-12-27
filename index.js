0645ace28ff5ea1791bebdbc3c01186d6e7bd93c
chetan <ctidgam1997@gmail.com>
2022-12-27 22:24:50 +0530

    changes

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const connect = require('./db/db')
 const cors = require('cors')
 
 const app = express()
-app.use(express.urlencoded({extended:false}))
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  })
-app.use(cors({origin:["http://localhost:3000","https://melodious-twilight-b98265.netlify.app"],credentials:true}))
-app.use(express.json())
+app.use(function(req, res, next) {  
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors({origin:true,credentials:true}));
 app.use(require("./routes/route"))
 app.listen('3001',async()=>{
     await connect()
